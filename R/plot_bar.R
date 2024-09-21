cb88d27dcc445ba68b2a0f73b800b3de8e728250
xwchen <xchenat@connect.ust.hk>
2024-09-22 03:48:27 +0800

    Remove legend

--- R/plot_bar.R
+++ R/plot_bar.R
@@ -48,7 +48,9 @@ plot_bar <- function(df, var, group, xlab, ylab) {
     ggplot2::xlab(xlab) +
     ggplot2::ylab(ylab) +
     ggplot2::scale_fill_brewer(palette = "Dark2") +
-    ggplot2::theme_bw()
+    ggplot2::theme_bw()+
+    # remove legend
+    ggplot2::theme(legend.position = "none")
 
   return(plot)
 }
